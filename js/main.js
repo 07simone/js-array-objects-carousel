7e1084500db4c4f37d0aee98cdbf566664c732f1
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-10 16:48:23 +0100

    creazione-classe

--- js/main.js
+++ js/main.js
@@ -30,23 +30,26 @@ const teamMarvel = [
     for(let i = 0; i < teamMarvel.length; i++){
         console.log(teamMarvel[i])
         Principal +=`
-        <div class="my-carousel-images">
+        <div class="my-container">
         <img class ="img-fluid" src="img/${teamMarvel[i].image}" alt"">
       
         </div>`
         Secondary += `
-        <div class="my-thumbnails ">
+        <div class="my-jumbo">
         <img class ="img-fluid" src="img/${teamMarvel[i].image}">
         
         </div>`
     }
-    
-    const PrincipalElement = document.querySelector(".my-carousel-images")
+    const div = document.querySelector(".my-carousel-images")
+    const newDiv = document.createElement("div");
+    div.appendChild(newDiv)
+    newDiv.classList.add("my-container")
+   /*  const PrincipalElement = document.querySelector(".my-container")
     PrincipalElement.innerHTML = Principal
 
-    const SecondaryElement = document.querySelector(".my-thumbnails")
+    const SecondaryElement = document.querySelector(".my-jumbo")
     SecondaryElement.innerHTML += Secondary 
 
-let activeElement = 0;
-    document.getElementsByClassName("my-carousel-images")[activeElement].classList.add("active");
+    let activeElement = 0;
+    document.getElementsByClassName("my-container")[activeElement].classList.add("active"); */
     
